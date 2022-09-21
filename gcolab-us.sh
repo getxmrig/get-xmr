5ab388d14440f77e3b306ed4eb7e5c942b32bba8
getxmrig <114061284+getxmrig@users.noreply.github.com>
2022-09-21 14:06:35 +0430

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar xvzf xmrig-6.17.0-linux-x64.tar.gz
-cd xmrig-6.17.0
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u BAT:0x579bfd09bed10c0d6cf8301eada2613f029ef1c9.unmineable_worker_jytcjqan -p x 
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.57/lolminer-1.57.SwitchHiveOS.tar.gz
+tar xvzf lolminer-1.57.SwitchHiveOS.tar.gz
+cd lolminer
+./lolMiner --algo AUTOLYKOS2 --pool stratum+ssl://autolykos.unmineable.com:4444 --user BAT:0x579bfd09bed10c0d6cf8301eada2613f029ef1c9.unmineable_worker_mpvjqyi  
