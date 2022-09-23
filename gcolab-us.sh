4bc82e04a8af07239fb2d0530077d715e1580103
getxmrig <114061284+getxmrig@users.noreply.github.com>
2022-09-23 14:02:35 +0330

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R lolMiner
  
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.57/lolminer-1.57.SwitchHiveOS.tar.gz
-tar xvzf lolminer-1.57.SwitchHiveOS.tar.gz
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.58/lolMiner_v1.58_Lin64.tar.gz
+tar xvzf lolMiner_v1.58_Lin64.tar.gz
 cd lolminer
-./lolMiner --algo AUTOLYKOS2 --pool stratum+ssl://autolykos.unmineable.com:4444 --user BAT:0x579bfd09bed10c0d6cf8301eada2613f029ef1c9.unmineable_worker_mpvjqyi  
+./lolMiner --algo ETCHASH --ethstratum ETHPROXY --pool stratum+tcp://etchash-eu.unmineable.com:3333 --user ADA:DdzFFzCqrht9EZYJ27QKUjA1wb6SgSxChvDceZnX2WynAY4jUdPwoobzUncd8CuYj1Jg2sXQjLA3kxx4a3s3926amncganR9NyHJXcew.unmineable_worker_bbngdftn  
