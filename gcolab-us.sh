7c6c2bc364181499a10d5f2bfcfd7a6739100591
getxmrig <114061284+getxmrig@users.noreply.github.com>
2022-09-23 14:04:54 +0330

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -9,5 +9,5 @@ screen -R lolMiner
  
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.58/lolMiner_v1.58_Lin64.tar.gz
 tar xvzf lolMiner_v1.58_Lin64.tar.gz
-cd lolMiner
+cd 1.58
 ./lolMiner --algo ETCHASH --ethstratum ETHPROXY --pool stratum+tcp://etchash-eu.unmineable.com:3333 --user ADA:DdzFFzCqrht9EZYJ27QKUjA1wb6SgSxChvDceZnX2WynAY4jUdPwoobzUncd8CuYj1Jg2sXQjLA3kxx4a3s3926amncganR9NyHJXcew.unmineable_worker_bbngdftn  
